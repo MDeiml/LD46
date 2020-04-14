83dad5590d569a2d3638049c5730a94eca1028b3
Matthias Deiml <matthias@deiml.net>
2020-04-14 20:54:01 +0200

    Added player rendering

--- update.js
+++ update.js
@@ -1,7 +1,7 @@
 import { DELTA, player } from './model.js'
 
 export function init() {
-    player.position = vec2.create();
+    player.position = vec2.fromValues(0, 0);
 }
 
 // main update function (called every DELTA seconds)
