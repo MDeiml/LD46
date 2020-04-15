73e56e769796b4a4ab4cd550fbeadc6f934db3da
Matthias Deiml <matthias@deiml.net>
2020-04-15 11:01:08 +0200

    Input example

--- update.js
+++ update.js
@@ -1,4 +1,5 @@
 import { DELTA, player } from './model.js'
+import { key } from './input.js'
 
 export function init() {
     player.position = vec2.fromValues(0, 0);
@@ -7,4 +8,7 @@ export function init() {
 // main update function (called every DELTA seconds)
 export function update() {
 	// hi its me
+    if (key('KeyD')) {
+        vec2.add(player.position, player.position, vec2.fromValues(DELTA, 0));
+    }
 }
