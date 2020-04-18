d5cd342c88496de83387ddcbfda8d036657df2b7
Matthias Deiml <matthias@deiml.net>
2020-04-18 10:52:55 +0200

    Added item and player to model

--- model.js
+++ model.js
@@ -4,6 +4,11 @@ export const FPS = 60;
 export const FRAME_TIME = 1000 / FPS;
 export const DELTA = 1 / FPS;
 
+export let items = [];
+export let player = {
+    position: vec2.create();
+}
+
 export function setCanvas(c) {
     canvas = c;
     gl = canvas.getContext('webgl');
